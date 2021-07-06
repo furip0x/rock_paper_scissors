15ad030bdb7f5e40ff2353dc60999a18c4d90c83
LordHelix <furkanipekci@hotmail.com>
2021-07-06 17:39:26 +0300

    Add everything..

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,96 @@
-let game = () => {
-
-  let playerScore = 0;
+const game = () => {
   let computerScore = 0;
+  let playerScore = 0;
 
-  let computerPlay = () => {
-    const computerOptions = ['Rock', 'Paper', 'Scissors'];
-    return computerOptions[Math.floor(Math.random() * computerOptions.length)];
+  const playRound = () => {
+    const playerButtons = document.querySelectorAll('.button');
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
+    playerButtons.forEach(button => {
+      button.addEventListener('click', function () {
+        const computerSelection = computerOptions[Math.floor(Math.random() * 3)];
+        const playerSelection = button.getAttribute('data-selection');
+
+        compare(playerSelection, computerSelection);
+      });
+    });
   };
 
-  let playerPlay = () => {
-    playerSelectionText = prompt('"Rock","Paper", "Scissors?"').toLowerCase();
-    const playerSelectionFirstChar = playerSelectionText.charAt(0).toUpperCase();
-    return playerSelectionFirstChar + playerSelectionText.slice(1);
-  }
+  const updateScores = () => {
+    const playerScoreText = document.querySelector('.user .user-score-number');
+    const computerScoreText = document.querySelector('.computer .user-score-number');
+    const winnerText = document.querySelector('.result-title');
 
-  let playRound = (playerSelection, computerSelection) => {
-    for (let i = 0; i < 5; i++) {
-      playerSelection = playerPlay();
-      computerSelection = computerPlay();
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
 
-      console.log(`Player: ${playerSelection} || Computer: ${computerSelection}`)
+    if(playerScore === 3) {
+      winnerText.textContent = 'WINNER PLAYER';
+      document.querySelector('body').classList.add('gameover');
+    } else if (computerScore === 3) {
+      winnerText.textContent = 'WINNER COMPUTER';
+      document.querySelector('body').classList.add('gameover');
+    }
+  };
+
+  const compare = (playerSelection, computerSelection) => {
+    const winner = document.querySelector('.result-title');
+
+    if (playerSelection === computerSelection) {
+      winner.textContent = "It's a tie";
+      return;
     }
 
-    return "You Lose! Paper beats Rock";
+    if (playerSelection === 'rock') {
+      if (computerSelection === 'scissors') {
+        winner.textContent = 'Player Wins';
+        playerScore++
+        updateScores();
+        return;
+      } else {
+        winner.textContent = 'Computer Wins';
+        computerScore++;
+        updateScores();
+        return;
+      }
+    }
+
+    if (playerSelection === 'paper') {
+      if (computerSelection === 'rock') {
+        winner.textContent = 'Player Wins';
+        playerScore++
+        updateScores();
+        return;
+      } else {
+        winner.textContent = 'Computer Wins';
+        computerScore++;
+        updateScores();
+        return;
+      }
+    }
+
+    if (playerSelection === 'scissors') {
+      if (computerSelection === 'paper') {
+        winner.textContent = 'Player Wins';
+        playerScore++
+        updateScores();
+        return;
+      } else {
+        winner.textContent = 'Computer Wins';
+        computerScore++;
+        updateScores();
+        return;
+      }
+    }
   };
 
-  playRound();
+  const playAgain = document.querySelector('.play-again-btn');
+  
+  playAgain.addEventListener('click', function(){
+    location.reload();
+  });
 
+  playRound();
 };
 
-game()
+game();
